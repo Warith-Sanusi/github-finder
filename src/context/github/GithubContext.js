4f0ab761bae5750d74a17b64b92fc338a351444a
Warith-Sanusi <kolawoledon20@gmail.com>
2022-10-03 12:25:19 +0100

    github-finder

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,81 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL 
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN 
+
+export const GithubProvider = ({children}) => {
+  const initialState = {
+    users:[],
+    user: {},
+    loading: false,
+  }
+
+  const [state, dispatch] = useReducer(githubReducer,initialState)
+
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      q: text
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, { 
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      },
+    })
+
+    const {items} = await response.json()
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    })
+  }
+
+  // Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, { 
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      },
+    })
+    
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
+  // clear users from staet
+  const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+
+  // Set loading
+  const setLoading = () => dispatch({type:'SET_LOADING'})
+
+  return <GithubContext.Provider value={{
+    users: state.users,
+    loading: state.loading,
+    user: state.user,
+    searchUsers, 
+    clearUsers,
+    getUser,
+  }}>
+    {children}
+  </GithubContext.Provider>
+
+}
+
+export default GithubContext
